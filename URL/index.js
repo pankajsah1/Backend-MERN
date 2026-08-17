604bc507a501d0ead88a28e4f0d7e3e848631e3f
pankaj <pankajkumarsah940@gmail.com>
2026-08-17 16:57:36 +0530

    HTTP methods in node.js

--- URL/index.js
+++ URL/index.js
@@ -1,17 +1,17 @@
 const http = require("http");
 const fs = require("fs");
-
 const url = require("url");
 
 const myServer = http.createServer((req, res) => {
     // console.log("response created")
     if(req.url === "/favicon.ico") return res.end();
-    const log = `${Date.now()}: ${req.url} new request recieved \n `;
+    const log = `${Date.now()}: ${req.url}: ${req.method} new request recieved \n `;
     const myUrl = url.parse(req.url, true);
     console.log(myUrl);
     fs.appendFile("log.text",log, (err, data) => {
         switch(myUrl.pathname){
-            case '/': res.end("WelCome to Home Page");
+            case '/':
+                if(req.method === 'GET')  res.end("WelCome to Home Page");
             break;
             case '/about': 
               const username = myUrl.query.myname;
@@ -19,7 +19,13 @@ const myServer = http.createServer((req, res) => {
             break;
             case '/search':
                 const search = myUrl.query.search_query;
-                res.end("Here are your results for" + search);
+                res.end("Here are your results for " + search);
+                case '/signup':
+                    if(req.method === 'GET') res.end("This is signup form")
+                    else if(req.method === 'POST'){
+                    // DB Query
+                    res.end("Success");
+                }
             default: res.end("404 Not found");
         }
     })
