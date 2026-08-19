1bad4bcb478d120a09111b4fb672dbe06ca6c03d
pankaj <pankajkumarsah940@gmail.com>
2026-08-20 00:23:01 +0530

    Building REST API's using Node and Express.js

--- Project_01/index.js
+++ Project_01/index.js
@@ -5,8 +5,36 @@ const app = express();
 const PORT = 8000;
 
 // Routes
+app.get('/users', (req, res) => {
+    const html = `
+    <ul>
+    ${users.map(user => `<li>${user.first_name}</li>`).join("")}
+    </ul>`;
+    res.send(html)
+})
+
+// REST API
 app.get('/api/users', (req, res)=>{
    return res.json(users)
+})
+
+app.route('/api/users/:id').get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id)
+    return res.json(user)
+})
+.patch((req, res)=>{
+    // TODO: Edit the user with id
+    return res.json({status: "pending"})
+})
+.delete((req, res) => {
+    // TODO: delete the user with id
+    return res.json({status: "pending"})
+})
+
+app.post('/api/users', (req, res) => {
+    // TODO: Create new user
+    return res.json({status: "pending"})
 })
 
 
