b7306c5509ee7104815768af179d0688333713e4
pankaj <pankajkumarsah940@gmail.com>
2026-08-21 21:35:38 +0530

    HTTP Status code

--- Project_01/index.js
+++ Project_01/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const users = require("./MOCK_DATA.json");
 const fs = require('fs');
 const { json } = require("stream/consumers");
+const { error } = require("console");
 
 const app = express();
 const PORT = 8000;
@@ -40,13 +41,14 @@ app.get('/api/users', (req, res)=>{
     // console.log('i am in get route', req.myUserName);   
    res.setHeader('X-myName', 'Pankaj Sah')         // Custom Header
     // Always add X to custom headers
-   console.log(req.headers)
+//    console.log(req.headers)
    return res.json(users)
 })
 
 app.route('/api/users/:id').get((req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id)
+    if(!user) return res.status(404).json({error: "User Not Found"})
     return res.json(user)
 })
 .patch((req, res)=>{
@@ -61,11 +63,14 @@ app.route('/api/users/:id').get((req, res) => {
 app.post('/api/users', (req, res) => {
     // TODO: Create new user
     const body = req.body;
+    if(!body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title){
+        return res.status(400).json({msg: "All Fiels are required!"})
+    }
     users.push({...body, id: users.length+1});
     fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data)=>{
-        return res.json({status: "success", id: users.length})
+        return res.status(201).json({status: "success", id: users.length})
     })
 })
 
 
-app.listen(PORT, ()=> console.log(`Server start rendering`))
+app.listen(PORT, ()=> console.log(`Server started at PORT:${PORT}`))
